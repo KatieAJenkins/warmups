3f9a42a6410182aad20c7a049b235b71c33e176f
KatieAJenkins <kind2karma@gmail.com>
2017-01-09 09:36:53 -0700

    initial commit with js function working

---
+++ 01092017.js
@@ -0,0 +1,29 @@
+'use strict';
+//Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each. Write tests instead of console.logs
+
+//IN - array
+//OUT - number
+
+// plusOneSum([1, 2, 3, 4]); // 14
+
+var array = [1, 2, 3, 4];
+var newArray = [];
+var sum = 0;
+
+function plusOneSum (array) {
+  //loop through array
+  for (var i = 0; i < array.length; i++) {
+    //add one to each number
+    //push to new array
+    newArray.push(array[i] += 1);
+    // console.log(newArray);
+  }
+    for(var j = 0; j < newArray.length; j++) {
+      //sum all numb
+      sum += array[j];
+      console.log(sum);
+    }
+  console.log(array);
+};
+
+plusOneSum(array);

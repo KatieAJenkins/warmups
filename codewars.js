480d9a8453d75eb012e6cceb987bd703df967fb3
KatieAJenkins <kind2karma@gmail.com>
2017-02-07 22:12:51 -0700

    added cooldown exercise for js fundamentals

--- codewars.js
+++ codewars.js
@@ -1,15 +1,37 @@
 'use strict';
 
+//create new array with only 4 char strings
 var friends = ["Ryan", "Kieran", "Jason", "Yous"];
 var bestFriends = [];
 
 function myFriends(array) {
   for (var i = 0; i < array.length; i++) {
-    if(friends[i].length <= 4) {
-      bestFriends.push(friends[i]);
-      // console.log(bestFriends);
+    if(array[i].length <= 4) {
+      bestFriends.push(array[i]);
+      console.log(bestFriends);
     }
   }
 }
 
 myFriends(friends);
+//
+// 'use strict';
+//
+// var friends = ["Ryan", "Kieran", "Jason", "Yous"];
+//
+// function filterFriends(value) {
+//   return value <= 4;
+// }
+//
+// var filteredFriends = ["Ryan", "Kieran", "Jason", "Yous"].filter(filterFriends);
+//
+// console.log(filteredFriends);
+// //   for (var i = 0; i < array.length; i++) {
+// //     if(friends[i].length <= 4) {
+// //       bestFriends.push(friends[i]);
+// //       // console.log(bestFriends);
+// //     }
+// //   }
+// // }
+//
+// // myFriends(friends);
---
+++ cooldown.js
@@ -0,0 +1,53 @@
+'use strict';
+//******* EXAMPLE: What will be logged when this code runs?
+var hello = 'world';
+// console.log('Hello, ' + hello); //'Hello world';
+
+//******* 1. What will be logged when this code runs?
+var users = ['Michelle', 'Hillary', 'Barbara', 'Rosalynn'];
+
+// console.log(users[2]); //'Barbara'
+// console.log(users[0]); //'Michelle'
+// console.log(users.length); //4
+
+//******* 2. What will be logged when this code runs?
+var mountains = ['Pike\'s Peak', 'Long\'s Peak', 'Mount Elbert', 'Mount Sneffels', 'Uncompaghre'];
+mountains.push('Mount Evans');
+// console.log(mountains); //var mountains = ['Pike\'s Peak', 'Long\'s Peak', 'Mount Elbert', 'Mount Sneffels', 'Uncompaghre', 'Mount Evans'];
+mountains.pop(); //var mountains = ['Pike\'s Peak', 'Long\'s Peak', 'Mount Elbert', 'Mount Sneffels', 'Uncompaghre'];
+// console.log(mountains);
+//removes the first value in array
+mountains.shift(); //var mountains = ['Long\'s Peak', 'Mount Elbert', 'Mount Sneffels', 'Uncompaghre'];
+
+
+//******* 3. given the input and output, what is the code?
+var fruits = ['apple', 'orange', 'banana', 'kiwi', 'grapfruit', 'watermelon'];
+
+console.log(fruits); //should print ['apple', 'banana', 'grapefruit', 'kiwi', 'orange', 'watermelon'];
+
+// var vegetables = ['potato', 'celery', 'spinach'];
+//TODO: write code here
+// console.log(fruitsAndVeggies); //should print ['apple', 'banana', 'grapefruit', 'kiwi', 'orange', 'watermelon', 'celery', 'potato', 'spinach'];
+
+
+
+
+//******* 4. What will be logged when this code runs?
+
+// for (var i = 0; i < vegetables.length; i++){
+//   console.log(vegetables[i]);
+// }
+
+//WRITE ANSWER HERE
+
+
+
+
+
+//******* BONUS: What will happen when this code runs?
+// counter = 9;
+//
+// while (counter < 10) {
+//   counter -= 1;
+// }
+//WRITE ANSWER HERE

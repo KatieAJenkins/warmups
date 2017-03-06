f5c26aedcecbdf12178ed580135b3d4b5aa11c02
KatieAJenkins <kind2karma@gmail.com>
2017-03-06 09:53:47 -0700

    added 2 more ways to find largest number

--- reduce.js
+++ reduce.js
@@ -5,24 +5,32 @@
 /////Find Largest of Numbers///////
 var numbers = [1,5,2,334,5,10];
 
+// function findLargest(numbers){
+  // var max = numbers[0];
+  // for(var i = 0; i < numbers.length; i++) {
+  //   if (numbers[i] > max){
+  //     max = numbers[i];
+  //   }
+  // }
+  // return max;
+// }
+
+// console.log(findLargest(numbers));
+
+// function findLargest(numbers){
+//   numbers = numbers.sort(function(x,y) {
+//     return y - x ;
+//   });
+//     return numbers[0];
+// }
+
 function findLargest(numbers){
-  var max = numbers[0];
-  for(var i = 0; i < numbers.length; i++) {
-    if (numbers[i] > max){
-      max = numbers[i];
+  return numbers.reduce(function(previous, current) {
+    if(current > previous) {
+      return current;
     }
-  }
-  return max;
+      return previous;
+  });
 }
 
 console.log(findLargest(numbers));
-
-function findLargest(numbers){
-  var max = numbers[0];
-  for(var i = 0; i < numbers.length; i++) {
-    if (numbers[i] > max){
-      max = numbers[i];
-    }
-  }
-  return max;
-}

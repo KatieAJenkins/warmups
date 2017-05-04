5899f7230b8df9c8b8bc8041f1ad15faf81686ec
KatieAJenkins <kind2karma@gmail.com>
2017-05-03 21:55:10 -0600

    cleaned up code

--- rotateArray.js
+++ rotateArray.js
@@ -3,7 +3,6 @@
 // Rotate an array arr in place arr to the right by k steps.
 
 // For example, with n = [1,2,3,4,5,6,7] and k = 3, the array is rotated to [5,6,7,1,2,3,4].
-//Make sure not to return a copy of the array.
 
 var array = [1,2,3,4,5,6,7];
 
@@ -21,21 +20,18 @@ var array = [1,2,3,4,5,6,7];
 
 toRight(array, 3);
 
+//not mutating original array
+//log array before
+//log array after
+//pass number is as the number of times you want to move the number over
+
 function toRight(array, number){
-  let steppedArray = [];
   for(var i = 0; i < number; i++){
-    console.log(array);
     // console.log(array);
-    // console.log(steps);
     // console.log(array.pop(array[i]));
     var popped = array.pop(array[i]);
-    console.log(popped);
+    // console.log(popped);
     array.unshift(popped);
-    console.log(steppedArray);
-    console.log(array);
+    // console.log(array);
   }
-  // let popped
-
-  // console.log(steppedArray);
-  // return steppedArray;
 }
